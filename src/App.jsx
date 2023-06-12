c41607f3c14b34f1a7694547d4554965de1beb05
keyxcode <keyxcode@gmail.com>
2023-06-12 14:25:46 +0700

    Implement basic gameplay

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,84 @@ import { useState } from "react";
 import Cell from "./components/Cell";
 
 function App() {
-  const [selected, setSelected] = useState([false, false, false, false]);
-  const [pitches, setPitches] = useState(["C3", "C4", "C3", "C4"]);
+  const [cells, setCells] = useState([
+    { selected: false, pitch: "C3", guessed: false },
+    { selected: false, pitch: "C4", guessed: false },
+    { selected: false, pitch: "C3", guessed: false },
+    { selected: false, pitch: "C4", guessed: false },
+  ]);
 
   const handleSelect = (id) => {
-    if (selected[id] === true) return;
+    if (cells[id].selected === true) return;
 
-    if (selected.filter((el) => el === true).length < 2) {
-      const newSelected = selected.map((value, i) =>
-        i === id ? !value : value
-      );
-      setSelected(newSelected);
-      return;
+    const newCells = cells.map((cell, i) =>
+      i === id ? { ...cell, selected: true } : cell
+    );
+    setCells(newCells);
+
+    const selectedCells = newCells.filter((cell) => cell.selected === true);
+    const numSelectedCells = selectedCells.length;
+    console.log("numSelectedCells", numSelectedCells);
+
+    if (numSelectedCells === 1) return;
+
+    if (numSelectedCells === 2) {
+      if (guessesAreCorrect(selectedCells)) {
+        const cellsMarkedCorrect = markSelectedCellsCorrect(newCells);
+        return checkGameOver(cellsMarkedCorrect);
+      } else {
+        return setAllCellsUnselected();
+      }
     }
 
-    const newSelected = [false, false, false, false];
-    newSelected[id] = true;
-    setSelected(newSelected);
+    return console.log(
+      `something wrong happened: num selected =${numSelectedCells}`
+    );
   };
 
-  const checkSamePitch = () => {};
+  const guessesAreCorrect = (selectedCells) => {
+    if (selectedCells[0].pitch === selectedCells[1].pitch) return true;
+    return false;
+  };
+
+  const markSelectedCellsCorrect = (cells) => {
+    const newCells = cells.map((cell) =>
+      cell.selected === true
+        ? { ...cell, guessed: true, selected: false }
+        : { ...cell, selected: false }
+    );
+
+    setTimeout(() => {
+      setCells(newCells);
+    }, 500);
+
+    return newCells;
+  };
+
+  const setAllCellsUnselected = () => {
+    const newCells = cells.map((cell) => ({ ...cell, selected: false }));
+    return setTimeout(() => {
+      setCells(newCells);
+    }, 500);
+  };
+
+  const checkGameOver = (cells) => {
+    const numGuessedCells = cells.filter(
+      (cell) => cell.guessed === true
+    ).length;
+    if (numGuessedCells === cells.length) return console.log("you win");
+  };
 
   return (
     <>
-      {pitches.map((pitch, i) => (
+      {cells.map((cell, i) => (
         <Cell
           key={i}
           cellId={i}
           handleSelect={handleSelect}
-          selected={selected[i]}
-          sound={pitch}
+          selected={cell.selected}
+          pitch={cell.pitch}
+          guessed={cell.guessed}
         />
       ))}
     </>
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,17 +2,23 @@ import useSound from "use-sound";
 import C3 from "../sounds/C3.mp3";
 import C4 from "../sounds/C4.mp3";
 
-const Cell = ({ cellId, handleSelect, selected, sound }) => {
-  const [play] = useSound(sound === "C3" ? C3 : C4);
+const Cell = ({ cellId, handleSelect, selected, pitch, guessed }) => {
+  const [play] = useSound(pitch === "C3" ? C3 : C4);
 
   const handleClick = () => {
+    if (guessed) return;
     handleSelect(cellId);
     play();
   };
 
   return (
-    <div onClick={handleClick} style={{ border: "1px solid red" }}>
-      {`I'm sound ${selected && "selected"}`}
+    <div
+      onClick={handleClick}
+      style={
+        guessed ? { border: "1px solid green" } : { border: "1px solid red" }
+      }
+    >
+      {`Cell ${cellId} with pitch ${pitch} selected = ${selected} guessed = ${guessed}`}
     </div>
   );
 };
