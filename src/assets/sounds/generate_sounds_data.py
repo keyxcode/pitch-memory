73dd0022068e7311ff82e92f302dcb3ffa618e10
keyxcode <keyxcode@gmail.com>
2023-06-25 01:00:40 +0700

    Create python script to generate soundData

---
+++ src/assets/sounds/generate_sounds_data.py
@@ -0,0 +1,25 @@
+import os
+
+with open("soundsData.ts", "w") as f:
+    # import all the sound files
+    sound_files = [file for file in os.listdir(".") if file.endswith(".mp3")]
+    note_names = []
+    for sound_file in sound_files:
+        # remove last 4 characters from sound_file (.mp3)
+        note_name = sound_file[:-4]
+        note_names.append(note_name)
+        f.write(f"import {note_name} from '../sounds/{note_name}.mp3';")
+
+    # import Sound typescript type
+    f.write("import { Sound } from '../../types';")
+
+    # create const sounds that contain note objects
+    f.write("const sounds: Sound[] = [")
+    note_objects = [
+        f"{{ path: {note_name}, name: '{note_name}' }}," for note_name in note_names
+    ]
+    for note_object in note_objects:
+        f.write(note_object)
+    f.write("];")
+
+    f.write("export default sounds;")
--- src/assets/sounds/soundsData.ts
+++ src/assets/sounds/soundsData.ts
@@ -1,56 +1,54 @@
 import C3 from "../sounds/C3.mp3";
-import Db3 from "../sounds/Db3.mp3";
-import D3 from "../sounds/D3.mp3";
-import Eb3 from "../sounds/Eb3.mp3";
+import B4 from "../sounds/B4.mp3";
+import C4 from "../sounds/C4.mp3";
+import C5 from "../sounds/C5.mp3";
+import B3 from "../sounds/B3.mp3";
+import D4 from "../sounds/D4.mp3";
 import E3 from "../sounds/E3.mp3";
+import D3 from "../sounds/D3.mp3";
+import E4 from "../sounds/E4.mp3";
+import Gb4 from "../sounds/Gb4.mp3";
+import F4 from "../sounds/F4.mp3";
+import Ab3 from "../sounds/Ab3.mp3";
+import G3 from "../sounds/G3.mp3";
+import Eb4 from "../sounds/Eb4.mp3";
+import Ab4 from "../sounds/Ab4.mp3";
 import F3 from "../sounds/F3.mp3";
 import Gb3 from "../sounds/Gb3.mp3";
-import G3 from "../sounds/G3.mp3";
-import Ab3 from "../sounds/Ab3.mp3";
-import A3 from "../sounds/A3.mp3";
-import Bb3 from "../sounds/Bb3.mp3";
-import B3 from "../sounds/B3.mp3";
-import C4 from "../sounds/C4.mp3";
-import Db4 from "../sounds/Db4.mp3";
-import D4 from "../sounds/D4.mp3";
-import Eb4 from "../sounds/Eb4.mp3";
-import E4 from "../sounds/E4.mp3";
-import F4 from "../sounds/F4.mp3";
-import Gb4 from "../sounds/Gb4.mp3";
+import Eb3 from "../sounds/Eb3.mp3";
 import G4 from "../sounds/G4.mp3";
-import Ab4 from "../sounds/Ab4.mp3";
+import Bb3 from "../sounds/Bb3.mp3";
+import A3 from "../sounds/A3.mp3";
+import Db4 from "../sounds/Db4.mp3";
+import Db3 from "../sounds/Db3.mp3";
 import A4 from "../sounds/A4.mp3";
 import Bb4 from "../sounds/Bb4.mp3";
-import B4 from "../sounds/B4.mp3";
-import C5 from "../sounds/C5.mp3";
 import { Sound } from "../../types";
-
 const sounds: Sound[] = [
   { path: C3, name: "C3" },
-  { path: Db3, name: "Db3" },
-  { path: D3, name: "D3" },
-  { path: Eb3, name: "Eb3" },
+  { path: B4, name: "B4" },
+  { path: C4, name: "C4" },
+  { path: C5, name: "C5" },
+  { path: B3, name: "B3" },
+  { path: D4, name: "D4" },
   { path: E3, name: "E3" },
+  { path: D3, name: "D3" },
+  { path: E4, name: "E4" },
+  { path: Gb4, name: "Gb4" },
+  { path: F4, name: "F4" },
+  { path: Ab3, name: "Ab3" },
+  { path: G3, name: "G3" },
+  { path: Eb4, name: "Eb4" },
+  { path: Ab4, name: "Ab4" },
   { path: F3, name: "F3" },
   { path: Gb3, name: "Gb3" },
-  { path: G3, name: "G3" },
-  { path: Ab3, name: "Ab3" },
-  { path: A3, name: "A3" },
-  { path: Bb3, name: "Bb3" },
-  { path: B3, name: "B3" },
-  { path: C4, name: "C4" },
-  { path: Db4, name: "Db4" },
-  { path: D4, name: "D4" },
-  { path: Eb4, name: "Eb4" },
-  { path: E4, name: "E4" },
-  { path: F4, name: "F4" },
-  { path: Gb4, name: "Gb4" },
+  { path: Eb3, name: "Eb3" },
   { path: G4, name: "G4" },
-  { path: Ab4, name: "Ab4" },
+  { path: Bb3, name: "Bb3" },
+  { path: A3, name: "A3" },
+  { path: Db4, name: "Db4" },
+  { path: Db3, name: "Db3" },
   { path: A4, name: "A4" },
   { path: Bb4, name: "Bb4" },
-  { path: B4, name: "B4" },
-  { path: C5, name: "C5" },
 ];
-
 export default sounds;
